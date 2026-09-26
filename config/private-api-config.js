a6b0b74c4a0fde76b483b74eab91b8238b676aee
Ivan Diaz <ivan@ivanadiaz.com>
2026-09-25 20:02:18 -0500

    Add one private portal auth endpoint

---
+++ config/private-api-config.js
@@ -0,0 +1,8 @@
+/**
+ * One internal portal login authority.
+ * Customer proposal authentication remains separate.
+ */
+(function (global) {
+  global.IVAN_PRIVATE_API_BASE =
+    'https://database2-api-xpxhayoxhq-vp.a.run.app/api/database2';
+})(typeof window !== 'undefined' ? window : globalThis);
